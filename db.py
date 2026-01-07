9b76b3d4818d8a1cac1d12e75b7dae6b7d7816fa
fer-vasco <fernandovasconi@hotmail.com>
2026-01-06 21:15:15 -0300

    Add files via upload
    Se agrega la función "Guardar_df_procesado_en_SQL"

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 tabla = "data/precios_SQL.db"
-
+datos_procesados = "data/precios_procesados_SQL.db"
 
 def Conectar_con_SQL(tabla_sql=tabla):
     return sqlite3.connect(tabla_sql)
@@ -26,3 +26,12 @@ def Cargar_base_de_datos(tabla_sql=tabla):
         return pd.read_sql(cadena, conn)
 
 
+def Guardar_df_procesado_en_SQL(dataframe_para_sql, tabla_sql=datos_procesados):
+    with Conectar_con_SQL(tabla_sql=datos_procesados) as conn:
+        dataframe_para_sql.to_sql(
+            tabla_sql,
+            conn,
+            if_exists="replace",  # o "replace"
+            index=False
+        )
+    print(f'df guardado en {tabla_sql}')
