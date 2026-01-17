e199147f9a2383762dc984fb4ef695a2298d9647
fer-vasco <fernandovasconi@hotmail.com>
2026-01-17 00:00:44 -0300

    Update Monitor_de_stocks_v13.py

--- Monitor_de_stocks_v13.py
+++ Monitor_de_stocks_v13.py
@@ -493,13 +493,13 @@ CHAT_ID = os.environ.get('CHAT_ID')
 API_KEY = os.environ.get('API_KEY')
 FMP_API_KEY = os.environ.get('FMP_API_KEY')
 
-'''
+
 # Reviso si el mercado está abierto. Si no, no hago nada
 mercado_abierto = Revisar_mercado(FMP_API_KEY, 'NASDAQ')
 if mercado_abierto == False:
     print('Mercado cerrado. Terminando el programa.')
     exit()
-'''
+
 
 monedas = [
 'FLS', 'TER', 'STX', 'MIR', 'GH', 
@@ -544,4 +544,5 @@ desempeño_separado = 'ℹ️ Desempeño separado: ' + str(round(desempeño_sepa
 alertas = Generar_alertas(df_procesado, 3)
 Enviar_mensaje(desempeño_separado, BOT_TOKEN, CHAT_ID)
 for alerta in alertas:
+
     Enviar_mensaje(alerta, BOT_TOKEN, CHAT_ID)
